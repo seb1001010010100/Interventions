12eca68eb2a74fdc9bfd84d654369826881e72dd
seb1001010010100 <36080429+seb1001010010100@users.noreply.github.com>
2018-03-24 12:08:05 -0400

    creation du test unitaire (une chaîne avec 10 espaces est invalide)

--- src/app/shared/caracteres-validator.spec.ts
+++ src/app/shared/caracteres-validator.spec.ts
@@ -2,10 +2,19 @@ import { VerifierEspaceValidator } from "./caracteres-validator";
 import { AbstractControl } from "@angular/forms";
 
 describe('sansEspaces Validator', () => {
-    
+
     it('une chaîne vide est invalide', () =>{
 
         let control = {value: ""};
+        let validator = VerifierEspaceValidator.sansEspace();
+        let result = validator(control as AbstractControl);
+        expect(result['prenom']).toBe(false);
+
+    });
+
+    it('une chaîne avec 10 espaces est invalide', () =>{
+
+        let control = {value: " ".repeat(10)};
         let validator = VerifierEspaceValidator.sansEspace();
         let result = validator(control as AbstractControl);
         expect(result['prenom']).toBe(false);
