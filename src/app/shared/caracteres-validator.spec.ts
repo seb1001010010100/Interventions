cabe5b4d3921373c3295ecd06f7492372b25a061
seb1001010010100 <36080429+seb1001010010100@users.noreply.github.com>
2018-03-24 12:07:36 -0400

    Creation du test unitaire (une chaîne vide est invalide)

--- src/app/shared/caracteres-validator.spec.ts
+++ src/app/shared/caracteres-validator.spec.ts
@@ -2,7 +2,15 @@ import { VerifierEspaceValidator } from "./caracteres-validator";
 import { AbstractControl } from "@angular/forms";
 
 describe('sansEspaces Validator', () => {
+    
+    it('une chaîne vide est invalide', () =>{
 
+        let control = {value: ""};
+        let validator = VerifierEspaceValidator.sansEspace();
+        let result = validator(control as AbstractControl);
+        expect(result['prenom']).toBe(false);
+
+    });
 
 
 });
